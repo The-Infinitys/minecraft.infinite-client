f09b0dd0b0922380f1a622292521ada37770151d
caleb <noobyetpro21@outlook.com>
2025-10-08 10:14:07 +0800

    Update Fighting.kt

--- src/client/kotlin/org/theinfinitys/features/Fighting.kt
+++ src/client/kotlin/org/theinfinitys/features/Fighting.kt
@@ -1,51 +1,46 @@
 package org.theinfinitys.features
 
 import org.theinfinitys.feature
-import org.theinfinitys.features.fighting.AutoTotem
-import org.theinfinitys.features.fighting.CounterAttack
-import org.theinfinitys.features.fighting.ImpactAttack
-import org.theinfinitys.features.fighting.KillAura
-import org.theinfinitys.features.fighting.NoAttack
-import org.theinfinitys.features.fighting.PlayerManager
-import org.theinfinitys.features.fighting.Reach
-import org.theinfinitys.features.fighting.SuperAttack
+import org.theinfinitys.features.fighting.*
+import org.theinfinitys.utils.translation.Translation
+
+
 
 val fighting =
     listOf(
         feature(
             "KillAura",
             KillAura(),
-            "近くのエンティティを自動的に攻撃します。",
+            Translation.t("fighting.KillAura.description"),
         ),
         feature(
             "Reach",
             Reach(),
-            "プレイヤーの到達距離を拡張し、より遠くのブロックやエンティティを操作できるようにします。",
+            Translation.t("fighting.Reach.description"),
         ),
         feature(
             "NoAttack",
             NoAttack(),
-            "村人やペットなどの特定のエンティティへの攻撃を防止します。",
+            Translation.t("fighting.NoAttack.description"),
         ),
         feature(
             "PlayerManager",
             PlayerManager(),
-            "フレンドと敵のプレイヤーを管理します。",
+            Translation.t("fighting.PlayerManager.description"),
         ),
         feature(
             "SuperAttack",
             SuperAttack(),
-            "自動でクリティカル攻撃を出します。",
+            Translation.t("fighting.SuperAttack.description"),
         ),
         feature(
             "CounterAttack",
             CounterAttack(),
-            "エンティティから攻撃を受けた際に、自動的に反撃します。",
+            Translation.t("fighting.CounterAttack.description"),
         ),
         feature(
             "ImpactAttack",
             ImpactAttack(),
-            "エンティティに連続攻撃を仕掛けます。",
+            Translation.t("fighting.ImpactAttack.description"),
         ),
-        feature("AutoTotem", AutoTotem(), "自動でトーテムを持ちます"),
     )
