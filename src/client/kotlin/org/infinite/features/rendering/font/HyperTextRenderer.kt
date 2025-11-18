5bbddfd5e41637a46b37a6fc91e3b08a474ce8c0
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-11-18 17:16:34 +0900

    TODO: complete implimentations

--- src/client/kotlin/org/infinite/features/rendering/font/HyperTextRenderer.kt
+++ src/client/kotlin/org/infinite/features/rendering/font/HyperTextRenderer.kt
@@ -1,27 +1,40 @@
 package org.infinite.features.rendering.font
 
+import net.minecraft.client.font.BakedGlyph
 import net.minecraft.client.font.FontManager
 import net.minecraft.client.font.TextRenderer
+import net.minecraft.text.Style
 import net.minecraft.util.Identifier
 import org.infinite.InfiniteClient
 
 class HyperTextRenderer(
-    val fontManager: FontManager,
+    fontManager: FontManager,
 ) : TextRenderer(fontManager.anyFonts) {
+    // 注入の必要性をチェックするメソッド
     private fun shouldInject(): Boolean = InfiniteClient.isFeatureEnabled(HyperFont::class.java)
 
+    // カスタムフォントの識別子を保持するクラス
     class HyperFonts(
-        regularIdentifier: Identifier,
-        italicIdentifier: Identifier,
-        boldIdentifier: Identifier,
-        italicBoldIdentifier: Identifier,
+        val regularIdentifier: Identifier,
+        val italicIdentifier: Identifier,
+        val boldIdentifier: Identifier,
+        val italicBoldIdentifier: Identifier,
     )
 
-    val hyperFonts =
+    // カスタムフォントのIdentifier
+    private val hyperFonts =
         HyperFonts(
-            Identifier.of("infinite", "infinite_regular"),
-            Identifier.of("infinite", "infinite_italic"),
-            Identifier.of("infinite", "infinite_bold"),
-            Identifier.of("infinite", "infinite_bolditalic"),
+            Identifier.of("minecraft", "infinite_regular"),
+            Identifier.of("minecraft", "infinite_italic"),
+            Identifier.of("minecraft", "infinite_bold"),
+            Identifier.of("minecraft", "infinite_bolditalic"),
         )
+
+    /**
+     * テキストのスタイルに基づいて使用するGlyphProviderを選択するロジック。
+     */
+    override fun getGlyph(
+        codePoint: Int,
+        style: Style,
+    ): BakedGlyph? = super.getGlyph(codePoint, style)
 }
