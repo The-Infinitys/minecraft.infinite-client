36d3b1486fbc47a47625d0111df682e0718ca62a
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-11-22 21:28:07 +0900

    feat: プロトコル偽装設定時にカスタムペイロードパケットをキャンセル プロトコル偽装設定が有効な場合、`CustomPayloadC2SPacket`の送信を明示的にキャンセルするように変更しました。 これにより、不要なパケット送信を防ぎ、プロトコル偽装の動作を改善します。 `Identifier`のインポートは不要になったため削除しました。

--- src/client/java/org/infinite/mixin/infinite/client/network/ClientConnectionMixin.java
+++ src/client/java/org/infinite/mixin/infinite/client/network/ClientConnectionMixin.java
@@ -3,7 +3,6 @@ package org.infinite.mixin.infinite.client.network;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
-import net.minecraft.util.Identifier;
 import org.infinite.InfiniteClient;
 import org.infinite.global.server.protocol.ProtocolSpoofingSetting;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,7 +19,7 @@ public class ClientConnectionMixin {
         InfiniteClient.INSTANCE.getGlobalFeature(ProtocolSpoofingSetting.class);
     if (protocolSpoofingSetting != null && protocolSpoofingSetting.isEnabled()) {
       if (packet instanceof CustomPayloadC2SPacket) {
-        Identifier channel = packet.getPacketType().id();
+        ci.cancel();
       }
     }
   }
